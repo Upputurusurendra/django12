650901cbe90749fb370179de9ed8cfb93c9e12f4
surendra <Upputurusurendra>
2021-11-22 16:10:43 +0530

    first time

---
+++ WebApp/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.urls import reverse
+# Create your models here.
+class Company(models.Model):
+    company_name=models.CharField(max_length=40)
+    company_logo=models.FileField(null='true',blank='True')
+    company_city=models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.company_name
+    def get_absolute_url(self):
+        return reverse('retrive',kwargs={"id":self.id})
+
+
